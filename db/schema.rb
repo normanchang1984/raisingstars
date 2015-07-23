649607a6ae27adb972a9f4b901b5b47d970719f8
HexterCH <hexter.ch@gmail.com>
2015-07-23 11:35:20 +0800

    basic Integrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722020201) do
+ActiveRecord::Schema.define(version: 20150722122937) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 20150722020201) do
   end
 
   add_index "comments", ["proposal_id"], name: "index_comments_on_proposal_id", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.string   "email",          limit: 255
+    t.string   "phone",          limit: 255
+    t.string   "address",        limit: 255
+    t.string   "payment_method", limit: 255
+    t.integer  "amount",         limit: 4
+    t.integer  "user_id",        limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name",       limit: 255
