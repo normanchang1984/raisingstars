3d268676cfefe4b3384bbec2aefdfc01f7654996
HexterCH <hexter.ch@gmail.com>
2015-07-27 17:54:24 +0800

    fix something

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,13 +2,14 @@ class Proposal < ActiveRecord::Base
   has_many :comments
   belongs_to :category
   belongs_to :user
+  accepts_nested_attributes_for :user
   has_many :userproposalships
   has_many :favorites, :through => :userproposalships, :source => :user
   has_many :products
   scope :sort_by_category, -> (category) { where( :category_id =>  category ) }
 
   #paperclip
-  has_attached_file :title_graph_url, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :title_graph_url, :styles => { :show => "848x364>", :index => "360x235>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :title_graph_url, :content_type => /\Aimage\/.*\Z/
 
   def page_count
