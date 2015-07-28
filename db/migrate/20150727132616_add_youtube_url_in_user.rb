3007e1fd96cfb881b328c4d5b8b4c8479576384e
HexterCH <hexter.ch@gmail.com>
2015-07-28 20:19:10 +0800

    allpay problem

--- db/migrate/20150727132616_add_youtube_url_in_user.rb
+++ db/migrate/20150727132616_add_youtube_url_in_user.rb
@@ -3,6 +3,7 @@ class AddYoutubeUrlInUser < ActiveRecord::Migration
     add_column :proposals, :youtube_url, :string
     add_column :proposals, :target, :integer
     add_column :proposals, :progress, :integer
+    add_column :proposals, :status, :boolean
     add_column :users, :fb_avatar_url, :string
   end
 end
