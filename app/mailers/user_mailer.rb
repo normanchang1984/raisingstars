3728e70db780d831d4434ea225cf482b2910dad8
HexterCH <hexter.ch@gmail.com>
2015-07-28 21:02:36 +0800

    for secure

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,28 @@
 class UserMailer < ApplicationMailer
-  default from: 'howclf@gmail.com'
+  default from: ''
+
   def welcome_email(user)
     @user = user
     @url = 'http://raisingstars.co/users/sign_in'
     mail(to: @user.email, subjec: 'Welcome to Raising Stars')
   end
+
+  def proposal_update(proposal)
+    @proposal = proposal
+  end
+
+  def proposal_progress(proposal, order)
+    @proposal = proposal
+    @user_order = order
+  end
+
+  def proposal_complete(proposal)
+    @proposal = proposal
+    @proposal_orders = @proposal.orders
+  end
+
+  def proposal_end(proposal)
+    @proposal = proposal
+  end
+
 end
