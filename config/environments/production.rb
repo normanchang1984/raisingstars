8c07f403228d4d5d81e32c39f6fca301430fac13
HexterCH <hexter.ch@gmail.com>
2015-07-29 20:43:37 +0800

    fix something

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,11 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
