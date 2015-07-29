3d5d4942ebd71b635133ac5b955497cc3cd29a6f
HexterCH <hexter.ch@gmail.com>
2015-07-29 20:43:37 +0800

    fix something

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -19,13 +19,15 @@ class Payment < ActiveRecord::Base
   def update_order_status
     if self.paid
       o = self.order
+      @orders = o.proposal.orders
       o.payment_status = "paid"
       o.proposal.progress += o.amount
-      o.proposal.
+
       if o.proposal.progress >= o.proposal.target
         UserMailer.delay.proposal_complete_owner(o.proposal)
-
-        UserMailer.delay.proposal_complete_user(o.proposal)
+        @orders.each do |p|
+          UserMailer.delay.proposal_complete_user(o.proposal, p.email)
+        end
       end
       o.save( :validate => false )
     end
