8080a42b58eb67148a800868e84c7b31ec2e4224
HexterCH <hexter.ch@gmail.com>
2015-07-30 11:05:46 +0800

    fix Allpay problem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,16 @@
 class UserMailer < ApplicationMailer
-
+  default :from => "poster@@raisingstar.co"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.proposal_complete_owner.subject
   #
-  def proposal_complete_owner
-    @greeting = "Hi"
+  def proposal_complete_owner(proposal)
+    owner_email = proposal.email
+    mail(:to => owner_email, :subject => "Good")
+  end
 
-    mail to: "to@example.org"
+  def proposal_complete_users(email)
+    mail(:to => email, :subject => "it work" )
   end
 end
