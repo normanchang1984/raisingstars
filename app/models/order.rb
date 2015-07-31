c4d03329433059127ec656b8c22dd6283ac2a507
HexterCH <hexter.ch@gmail.com>
2015-07-31 17:27:17 +0800

    ihower code review advise 7/30

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,11 @@
 class Order < ActiveRecord::Base
+
   has_many :line_items, :dependent => :destroy
   belongs_to :product
   belongs_to :proposal
   belongs_to :user
+
+  scope :paid, -> { where( :payment_status => "paid") }
 
   def paid?
     self.payment_status == "paid"
